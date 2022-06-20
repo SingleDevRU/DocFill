4603ee34f4a126ff3d9547b041e74b7a700ae94a
SingleDevRU <dimon333x22@outlook.com>
2022-06-20 21:08:09 +0500

    Version 1

---
+++ main.py
@@ -0,0 +1,455 @@
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.app import MDApp
+from kivymd.uix.tab import MDTabsBase
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivymd.uix.menu import MDDropdownMenu
+from kivymd.uix.picker import MDDatePicker
+
+import rq
+import dw
+
+
+def AddSymbol(string, value):
+    while len(string) != value:
+        string += " "
+
+    return string
+
+
+class OFD_and_DATA(MDFloatLayout, MDTabsBase):
+    pass
+
+
+class Address(MDFloatLayout, MDTabsBase):
+    pass
+
+
+class KKT_and_FN(MDFloatLayout, MDTabsBase):
+    pass
+
+
+class Primary(MDFloatLayout, MDTabsBase):
+    pass
+
+
+class CurrentKKT(Screen):
+    def on_save(self, instance, value, date_range):
+        date = str(value).split("-")
+        monthlist = ("января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября",
+                     "октября", "ноября", "декабря")
+        self.ids.Date.text = date[2] + " " + monthlist[int(date[1]) - 1] + " " + date[0]
+
+    def show_date_picker(self):
+        date = MDDatePicker()
+        date.elem = self.ids.Date.text
+        date.bind(on_save=self.on_save)
+        date.open()
+
+    def RegN(self):
+        self.manager.get_screen("Doc").ids.curkkt.text = self.ids.reg.text + "/" + self.ids.Date.text
+        self.manager.current = "Doc"
+
+
+class DocFill(Screen):
+    def request(self, inn):
+        info = rq.EgrulRq(inn)
+        if info:
+            self.ids.ogrn.text = info[2]
+            if info[0] == "fl":
+                self.ids.name1.text = "ИНДИВИДУАЛЬНЫЙ ПРЕДПРИНИМАТЕЛЬ"
+                self.ids.name2.text = info[1]
+            else:
+                self.ids.kpp.text = info[3]
+                if "ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ" in info[1]:
+                    self.ids.name1.text = info[1][:40]
+                    self.ids.name2.text = info[1][41:]
+                elif "ПУБЛИЧНОЕ АКЦИОНЕРНОЕ ОБЩЕСТВО" in info[1]:
+                    self.ids.name1.text = info[1][:30]
+                    self.ids.name2.text = info[1][31:]
+                elif "АКЦИОНЕРНОЕ ОБЩЕСТВО" in info[1]:
+                    self.ids.name1.text = info[1][:20]
+                    self.ids.name2.text = info[1][21:]
+
+    def MenuDock(self):
+        items = ["Регистрация", "Перерегистрация", "Снятие с учета"]
+        menu_items = [
+            {
+                "text": f'{i}',
+                "viewclass": "OneLineListItem",
+                "on_press": lambda x=f'{i}': self.MenuDock_callback(x)
+            } for i in items
+        ]
+        menu = MDDropdownMenu(
+            caller=self.ids.DockType,
+            items=menu_items,
+            width_mult=2.69,
+            max_height=150
+
+        )
+        menu.open()
+
+    def UserMenu(self):
+        items = ["Пользователь", "Представитель"]
+        menu_items = [
+            {
+                "text": f'{i}',
+                "viewclass": "OneLineListItem",
+                "on_press": lambda x=f'{i}': self.MenuUser_callback(x)
+            } for i in items
+        ]
+        menu = MDDropdownMenu(
+            caller=self.ids.User,
+            items=menu_items,
+            width_mult=2.5,
+            max_height=100,
+
+        )
+        menu.open()
+
+    def KKTMenu(self):
+        items = dw.GetKKT()
+        menu_items = [
+            {
+                "text": f'{i}',
+                "viewclass": "OneLineListItem",
+                "on_press": lambda x=f'{i}': self.KKTMenu_callback(x)
+            } for i in items if self.ids.KKT.text.lower() in i.lower()
+        ]
+        menu = MDDropdownMenu(
+            caller=self.ids.KKT,
+            items=menu_items,
+            width_mult=4,
+            max_height=300,
+            position="bottom",
+            ver_growth="down"
+
+        )
+        menu.open()
+
+    def FNMenu(self):
+        items = dw.GetFN()
+        menu_items = [
+            {
+                "text": f'{i}',
+                "viewclass": "OneLineListItem",
+                "on_press": lambda x=f'{i}': self.FNMenu_callback(x)
+            } for i in items.keys() if self.ids.FN.text.lower() in str(i).lower()
+        ]
+        menu = MDDropdownMenu(
+            caller=self.ids.FN,
+            items=menu_items,
+            width_mult=100,
+            max_height=300,
+            position="bottom",
+            ver_growth="down"
+
+        )
+        menu.open()
+
+    def OFDMenu(self):
+        items = dw.GetOFD()
+        menu_items = [
+            {
+                "text": f'{i}',
+                "viewclass": "OneLineListItem",
+                "on_press": lambda x=f'{i}': self.OFDMenu_callback(x)
+            } for i in items.keys() if self.ids.OFD.text.lower() in str(i).lower()
+        ]
+        menu = MDDropdownMenu(
+            caller=self.ids.OFD,
+            items=menu_items,
+            width_mult=100,
+            max_height=300,
+            position="bottom",
+            ver_growth="down"
+
+        )
+        menu.open()
+
+    def RegionMenu(self):
+        items = dw.GetRegions()
+        menu_items = [
+            {
+                "text": f'{i}',
+                "viewclass": "OneLineListItem",
+                "on_press": lambda x=f'{i}': self.RegionMenu_callback(x)
+            } for i in items if self.ids.region.text.lower() in str(i).lower()
+        ]
+        menu = MDDropdownMenu(
+            caller=self.ids.region,
+            items=menu_items,
+            width_mult=100,
+            max_height=300,
+            position="bottom",
+            ver_growth="down"
+
+        )
+        menu.open()
+
+    def OFDMenu_callback(self, text):
+        items = dw.GetOFD()
+        self.ids.OFD.text = text
+        self.ids.ofdn1.text = items[text][0][0] if items[text][0][0] is not None else ""
+        self.ids.ofdn2.text = items[text][0][1] if items[text][0][1] is not None else ""
+        self.ids.ofdn3.text = items[text][0][2] if items[text][0][2] is not None else ""
+        self.ids.ofdn4.text = items[text][0][3] if items[text][0][3] is not None else ""
+        self.ids.ofdinn.text = items[text][1] if items[text][1] is not None else ""
+
+    def RegionMenu_callback(self, text):
+        self.ids.region.text = text
+
+    def FNMenu_callback(self, text):
+        items = dw.GetFN()
+        self.ids.FN.text = text
+        self.ids.fn1.text = items[text][0] if items[text][0] is not None else ""
+        self.ids.fn2.text = items[text][1] if items[text][1] is not None else ""
+        self.ids.fn3.text = items[text][2] if items[text][2] is not None else ""
+        self.ids.fn4.text = items[text][3] if items[text][3] is not None else ""
+        self.ids.fn5.text = items[text][4] if items[text][4] is not None else ""
+        self.ids.fn6.text = items[text][5] if items[text][5] is not None else ""
+
+    def KKTMenu_callback(self, text):
+        self.ids.KKT.text = text
+
+    def MenuUser_callback(self, text):
+        self.ids.User.text = text
+
+    def MenuDock_callback(self, text):
+        self.ids.DockType.text = text
+        if text == "Перерегистрация":
+            self.manager.current = "KKT"
+
+    def on_save(self, instance, value, date_range):
+        date = str(value).split("-")
+        monthlist = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября",
+                     "октября", "ноября", "декабря"]
+        self.ids.Date.text = date[2] + " " + monthlist[int(date[1]) - 1] + " " + date[0]
+
+    def show_date_picker(self):
+        date = MDDatePicker()
+        date.elem = self.ids.Date.text
+        date.bind(on_save=self.on_save)
+        date.open()
+
+    def GetPostIndex(self):
+        if self.ids.district.text == "":
+            text = self.ids.city.text
+        else:
+            text = self.ids.district.text + "," + self.ids.village.text
+
+        self.ids.ZipCode.text = rq.GetIndex(text) if rq.GetIndex(text) is not False else ""
+
+    def GenerateXLSX(self):
+        data = {"Primary": {"TypeUser": (self.ids.TG1.active, self.ids.TG2.active),
+                            "TypeDock": self.ids.DockType.text, "CurKkt": self.ids.curkkt.text,
+                            "DateDock": self.ids.Date.text, "User": self.ids.User.text,
+                            "Resons": (self.ids.r1.active, self.ids.r2.active, self.ids.r3.active,
+                                       self.ids.r4.active, self.ids.r5.active, self.ids.r6.active,
+                                       self.ids.r7.active, self.ids.r8.active)
+                            },
+                "KKTandFN": {},
+                "Address": {"index": self.ids.ZipCode.text, "RegionCode": self.ids.region.text[:2]},
+                "OFD": {"Using": (self.ids.ofd1.active, self.ids.ofd2.active, self.ids.ofd3.active,
+                                  self.ids.ofd4.active, self.ids.ofd5.active, self.ids.ofd7.active,
+                                  self.ids.ofd6.active, self.ids.ofd12.active, self.ids.ofd8.active,
+                                  self.ids.ofd9.active, self.ids.ofd10.active, self.ids.ofd11.active)}
+                }
+        ogrn = self.ids.ogrn.text
+        if len(ogrn) < 15:
+            ogrn = AddSymbol(ogrn, 15)
+
+        data["Primary"]["OGRN"] = ogrn
+
+        inn = self.ids.inn.text
+        if len(inn) != 12:
+            inn = AddSymbol(inn, 12)
+
+        data["Primary"]["INN"] = inn
+
+        kpp = self.ids.kpp.text
+        if len(kpp) != 9:
+            kpp = AddSymbol(kpp, 9)
+
+        data["Primary"]["KPP"] = kpp
+
+        name1 = self.ids.name1.text
+        if len(name1) != 40:
+            name1 = AddSymbol(name1, 40)
+
+        name2 = self.ids.name2.text
+        if len(name2) != 40:
+            name2 = AddSymbol(name2, 40)
+
+        name3 = self.ids.name3.text
+        if len(name3) != 40:
+            name3 = AddSymbol(name3, 40)
+
+        data["Primary"]["FullName"] = (name1, name2, name3)
+
+        userf = self.ids.f.text
+        if len(userf) != 20:
+            userf = AddSymbol(userf, 20)
+
+        useri = self.ids.i.text
+        if len(useri) != 20:
+            useri = AddSymbol(useri, 20)
+
+        usero = self.ids.o.text
+        if len(usero) != 20:
+            usero = AddSymbol(usero, 20)
+
+        data["Primary"]["UserFIO"] = (userf, useri, usero)
+
+        doc1 = self.ids.d1.text
+        if len(doc1) != 20:
+            doc1 = AddSymbol(doc1, 20)
+
+        doc2 = self.ids.d2.text
+        if len(doc2) != 20:
+            doc2 = AddSymbol(doc2, 20)
+
+        doc3 = self.ids.d3.text
+        if len(doc3) != 20:
+            doc3 = AddSymbol(doc3, 20)
+
+        data["Primary"]["Document"] = (doc1, doc2, doc3)
+
+        model = self.ids.KKT.text
+        if len(model) != 20:
+            model = AddSymbol(model, 20)
+
+        data["KKTandFN"]["Model"] = model
+
+        KKTs = self.ids.seriesKKT.text
+        if len(KKTs) != 20:
+            KKTs = AddSymbol(KKTs, 20)
+
+        data["KKTandFN"]["SerialKKT"] = KKTs
+
+        FNs = self.ids.serialFN.text
+        if len(FNs) != 20:
+            FNs = AddSymbol(FNs, 20)
+
+        data["KKTandFN"]["SerialFN"] = FNs
+
+        FNname1 = self.ids.fn1.text
+        if len(FNname1) != 20:
+            FNname1 = AddSymbol(FNname1, 20)
+
+        FNname2 = self.ids.fn2.text
+        if len(FNname2) != 20:
+            FNname2 = AddSymbol(FNname2, 20)
+
+        FNname3 = self.ids.fn3.text
+        if len(FNname3) != 20:
+            FNname3 = AddSymbol(FNname3, 20)
+
+        FNname4 = self.ids.fn4.text
+        if len(FNname4) != 20:
+            FNname4 = AddSymbol(FNname4, 20)
+
+        FNname5 = self.ids.fn5.text
+        if len(FNname5) != 20:
+            FNname5 = AddSymbol(FNname5, 20)
+
+        FNname6 = self.ids.fn6.text
+        if len(FNname6) != 20:
+            FNname6 = AddSymbol(FNname6, 20)
+
+        data["KKTandFN"]["FNName"] = (FNname1, FNname2, FNname3, FNname4, FNname5, FNname6)
+
+        distr = self.ids.district.text
+        if len(distr) != 30:
+            distr = AddSymbol(distr, 30)
+
+        data["Address"]["District"] = distr
+
+        city = self.ids.city.text
+        if len(city) != 30:
+            city = AddSymbol(city, 30)
+
+        data["Address"]["City"] = city
+
+        vill = self.ids.village.text
+        if len(vill) != 30:
+            vill = AddSymbol(vill, 30)
+
+        data["Address"]["Village"] = vill
+
+        Street = self.ids.street.text
+        if len(Street) != 30:
+            Street = AddSymbol(Street, 30)
+
+        data["Address"]["Street"] = Street
+
+        house = self.ids.house.text
+        if len(house) != 8:
+            house = AddSymbol(house, 8)
+
+        data["Address"]["House"] = house
+
+        camp = self.ids.campus.text
+        if len(camp) != 8:
+            camp = AddSymbol(camp, 8)
+
+        data["Address"]["Campus"] = camp
+
+        room = self.ids.room.text
+        if len(room) != 8:
+            room = AddSymbol(room, 8)
+
+        data["Address"]["Room"] = room
+
+        place1 = self.ids.exp1.text
+        if len(place1) != 20:
+            place1 = AddSymbol(place1, 20)
+
+        place2 = self.ids.exp2.text
+        if len(place2) != 20:
+            place2 = AddSymbol(place2, 20)
+
+        place3 = self.ids.exp3.text
+        if len(place3) != 20:
+            place3 = AddSymbol(place3, 20)
+
+        data["Address"]["Place"] = (place1, place2, place3)
+
+        OFDName1 = self.ids.ofdn1.text
+        if len(OFDName1) != 20:
+            OFDName1 = AddSymbol(OFDName1, 20)
+
+        OFDName2 = self.ids.ofdn2.text
+        if len(OFDName2) != 20:
+            OFDName2 = AddSymbol(OFDName2, 20)
+
+        OFDName3 = self.ids.ofdn3.text
+        if len(OFDName3) != 20:
+            OFDName3 = AddSymbol(OFDName3, 20)
+
+        OFDName4 = self.ids.ofdn4.text
+        if len(OFDName4) != 20:
+            OFDName4 = AddSymbol(OFDName4, 20)
+
+        data["OFD"]["OFDName"] = (OFDName1, OFDName2, OFDName3, OFDName4)
+
+        OFDInn = self.ids.ofdinn.text
+        if len(OFDInn) != 12:
+            OFDInn = AddSymbol(OFDInn, 12)
+
+        data["OFD"]["OFDInn"] = OFDInn
+
+        dw.excelwork(data)
+
+
+class MyApp(MDApp):
+    def build(self):
+        self.load_kv("interface.kv")
+        sm = ScreenManager()
+        sm.add_widget(DocFill(name='Doc'))
+        sm.add_widget(CurrentKKT(name='KKT'))
+
+        return sm
+
+
+if __name__ == "__main__":
+    MyApp().run()
---
+++ rq.py
@@ -0,0 +1,42 @@
+import requests as rq
+from dadata import Dadata
+
+
+def EgrulRq(inn):
+    """
+    Запрос к ЕГРЮЛ для получения данных контрагента
+    """
+    info = []
+    if len(inn) == 0:
+        return
+    url = 'https://egrul.nalog.ru'
+    url1 = 'https://egrul.nalog.ru/search-result/'
+
+    r = rq.post(url, data={"query": inn})
+    if r:
+        r1 = rq.get(url1 + r.json()["t"])
+        if r1:
+            res = r1.json()["rows"][0]
+            # вид лица
+            info.append(res["k"])
+            # полное наименование/ФИО
+            info.append(res["n"])
+            # ОГРН
+            info.append(res["o"])
+            if res["k"] == "ul":
+                # КПП
+                info.append(res["p"])
+
+    return info if len(info) > 0 else False
+
+
+def GetIndex(text):
+    dadata = Dadata("211c3a81d71b207a60d6b8c287b59b9617b010bd", "8f549bc58baf074ebbfa28a0deac1b90158d8cb4")
+
+    res = dadata.suggest("postal_unit", f"{text}")
+
+    return res[0]["value"] if len(res) > 0 else False
+
+
+if __name__ == "__main__":
+    print(GetIndex("Г.ИРБИТ"))
